ae17f5987894eb11289cabdc88be4d991c1aee75
soukaina <78257019+soukainna@users.noreply.github.com>
2025-07-21 14:34:18 +0200

    script to run and test

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "🧹 Arrêt des anciens conteneurs..."
+docker-compose down
+
+echo "🔨 Rebuild des images..."
+docker-compose build
+
+echo "🚀 Démarrage des services..."
+docker-compose up -d
+
+echo "📄 Logs de l'API :"
+docker-compose logs -f task-service
---
+++ test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "🧪 Lancement des tests unitaires..."
+docker-compose run --rm tester
